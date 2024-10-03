74d4df8ae13569010bd82a1ce7e0882ccaf93235
johnbell2808 <johnbell2808@gmail.com>
2024-10-02 20:21:53 -0400

    Node JS Project - First Commit

---
+++ jsrefresher/play.js
@@ -0,0 +1,18 @@
+const name = 'Max';
+let age = 20;
+const hasHobbies = true;
+
+name = 'Maximillion';
+age = 30;
+
+function summarizeUser(userName, userAge, userHasHobby){
+    
+        return ('Name is ' 
+        + userName + ' age is ' 
+        + userAge + 
+        ' and the users hobbies: ' 
+        + userHasHobby);
+    
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
